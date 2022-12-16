fe8b846ea3269dbbdf62293dbd4f41ac100a0b95
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-16 23:43:49 +0530

    Update ping.py

--- Spoiled/SpoiledPlugins/ping.py
+++ Spoiled/SpoiledPlugins/ping.py
@@ -2,20 +2,23 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from Spoiled.Utils import eor
 import time
-from config import PING_PIC
+from config import IMAGES, STUFF
+
+hl = STUFF.COMMAND_HANDLER
 
 @Client.on_message(filters.command("ping", hl))
 async def alive_or_ping(_, m):
+    l = await _.get_me()
     st = time.time()
-    await eor(m, "`Checking...`")
+    ok = await eor(m, "`Checking...`")
     end = time.time()
     men = l.mention
     xD = ""
     xD += f"✥ 𝙊𝙬𝙣𝙚𝙧 :- {men}\n"
     xD += f"✥ 𝙋𝙞𝙣𝙜 :- {str((end-st)*1000)[0:5]}ms\n"
-    xD += f"✥ 𝙐𝙗 𝘿𝙚𝙫 :- [ЄƝƊ](t.me/THE_END_NETWORK)\n"
-    await m.delete()
+    xD += f"✥ 𝙐𝙗 𝘿𝙚𝙫 :- [𝚂𝙿𝙻](t.me/SpLBots)\n"
+    await ok.delete()
     try:
-        return await m.reply_photo(ALIVE_PIC, caption=xD)
+        return await m.reply_photo(IMAGES.PING_IMG, caption=xD)
     except:
         return await m.reply(xD)
