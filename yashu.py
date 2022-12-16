fcfbfdde717d21a0a796c9169cdd9f94e7cb0c5c
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-16 23:23:27 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,21 +1,18 @@
 from pyrogram import Client, idle
 from config import *
-import asyncio
 
 SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION, plugins=dict(root="Spoiled/SpoiledPlugins"))
-"""
-async def initiate():
-    await SPL.start()
-    x = await SPL.get_me()
+
+def initiate():
+    SPL.start()
+    x = SPL.get_me()
     try:
-        await SPL.join_chat("Coding_bots")
+        SPL.join_chat("Coding_bots")
     except:
         pass
     un = x.username
     print(f"@{un} started successfully !")
-    await idle()
+    idle()
 
-asyncio.run(initiate())
-"""
+initiate()
 
-SPL.run()
