a1f72fe5f937e696ba67b918707c3f20566ccaa5
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 00:38:45 +0530

    Create verify.py

---
+++ Spoiled/Utils/verify.py
@@ -0,0 +1,10 @@
+from Spoiled.Database.sudo import is_sudo
+
+async def verify(_, m):
+    x = m.from_user.is_self
+    if not x:
+        id = m.from_user.id
+        sudo = await is_sudo(id)
+        if not sudo:
+            return False
+    return True
