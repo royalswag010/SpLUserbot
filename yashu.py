224e6564a0961a66172209c4699758e9d8936bff
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-16 21:54:43 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -3,7 +3,7 @@ from config import *
 import asyncio
 
 SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION, plugins=dict(root="Spoiled/SpoiledPlugins"))
-
+"""
 async def initiate():
     await SPL.start()
     x = await SPL.get_me()
@@ -16,3 +16,6 @@ async def initiate():
     await idle()
 
 asyncio.run(initiate())
+"""
+
+SPL.run()
