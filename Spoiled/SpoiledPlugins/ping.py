a2585f1be5999a1642df739a59364796d50056df
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 00:14:01 +0530

    Update ping.py

--- Spoiled/SpoiledPlugins/ping.py
+++ Spoiled/SpoiledPlugins/ping.py
@@ -3,6 +3,8 @@ from pyrogram.types import Message
 from Spoiled.Utils import eor
 import time
 from config import IMAGES, STUFF
+from .afk import get_readable_time as grt
+from . import startTime
 
 hl = STUFF.COMMAND_HANDLER
 
@@ -15,11 +17,11 @@ TEXT = """
   
 ┃┃ ㅤ🧑‍💻ㅤㅤㅤρꪮꪀᧁ 
 ┃┃
-┃┃ㅤㅤㅤ𝕊𝔼ℕ𝕊𝔼𝕀 : {mention}
+┃┃ㅤㅤㅤ𝕊𝔼ℕ𝕊𝔼𝕀 : {}
 ┃┃
-┃┃ㅤ✅ 𝐔𝐏𝐓𝐈𝐌𝐄 : {uptime}
+┃┃ㅤ✅ 𝐔𝐏𝐓𝐈𝐌𝐄 : {}
 ┃┃
-┃┃ㅤ🚩{ping}𝐦𝐬 0.00 𝐦𝐬
+┃┃ㅤ🚩{}𝐦𝐬
 ┃┃
 ㅤ   ┗━━━ 💫🦋🦋💫━━━━┛
 . *       🌏          *
@@ -35,12 +37,14 @@ async def alive_or_ping(_, m):
     ok = await eor(m, "`Checking...`")
     end = time.time()
     men = l.mention
+    pong = str((end-st)*1000)[0:5]
+    grt = grt(int(time.time()-startTime))
     xD = ""
     xD += f"✥ 𝙊𝙬𝙣𝙚𝙧 :- {men}\n"
     xD += f"✥ 𝙋𝙞𝙣𝙜 :- {str((end-st)*1000)[0:5]}ms\n"
     xD += f"✥ 𝙐𝙗 𝘿𝙚𝙫 :- [𝚂𝙿𝙻](t.me/SpLBots)\n"
     await ok.delete()
     try:
-        return await m.reply_photo(IMAGES.PING_IMG, caption=xD)
+        return await m.reply_photo(IMAGES.PING_IMG, caption=TEXT.format(men, grt, pong))
     except:
-        return await m.reply(xD)
+        return await m.reply(TEXT.format(men, grt, pong))
