57c29f8a5bbb3c3e337b4590928295d6eb82e8d2
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 00:32:42 +0530

    Create get_id.py

---
+++ Spoiled/Utils/get_id.py
@@ -0,0 +1,17 @@
+from pyrogram.types import Message
+
+async def get_id(_, m: Message):
+    if m.from_user.is_self:
+        return None
+    if str(m.chat.id)[0] != "-":
+        return int(m.chat.id)
+    if not m.reply_to_message:
+        text = m.text.split()
+        un_or_id = text[1]
+        if un_or_id[0] == "@":
+            id = (await _.get_users(un_or_id)).id
+        else:
+            id = int(un_or_id)
+    else:
+        id = m.reply_to_message.from_user.id
+    return id 
