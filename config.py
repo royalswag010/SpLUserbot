2d4d46de8b3029d2aac37f2335077ceac198fef3
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-16 18:25:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+from os import getenv
+
+class API:
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
+
+class SECRETS:
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    STRING_SESSION = getenv("STRING_SESSION", "")
+
+class STUFF:
+    COMMAND_HANDLER = getenv("COMMAND_HANDLER", ".")
+
+class IMAGES:
+    ALIVE_IMG = getenv("ALIVE_IMG", "")
+    PING_IMG = getenv("PING_IMG", "")
+
+class DATABASE:
+    MONGO_DB_URL = getenv("MONGO_DB_URL", "")
+
