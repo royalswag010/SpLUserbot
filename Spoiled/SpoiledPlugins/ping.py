15107c171feb4ae4b2604422a81a7c81f728c1b0
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 00:20:27 +0530

    Update ping.py

--- Spoiled/SpoiledPlugins/ping.py
+++ Spoiled/SpoiledPlugins/ping.py
@@ -38,13 +38,13 @@ async def alive_or_ping(_, m):
     end = time.time()
     men = l.mention
     pong = str((end-st)*1000)[0:5]
-    grt = grt(int(time.time()-startTime))
+    gtr = grt(int(time.time()-startTime))
     xD = ""
     xD += f"✥ 𝙊𝙬𝙣𝙚𝙧 :- {men}\n"
     xD += f"✥ 𝙋𝙞𝙣𝙜 :- {str((end-st)*1000)[0:5]}ms\n"
     xD += f"✥ 𝙐𝙗 𝘿𝙚𝙫 :- [𝚂𝙿𝙻](t.me/SpLBots)\n"
     await ok.delete()
     try:
-        return await m.reply_photo(IMAGES.PING_IMG, caption=TEXT.format(men, grt, pong))
+        return await m.reply_photo(IMAGES.PING_IMG, caption=TEXT.format(men, gtr, pong))
     except:
-        return await m.reply(TEXT.format(men, grt, pong))
+        return await m.reply(TEXT.format(men, gtr, pong))
