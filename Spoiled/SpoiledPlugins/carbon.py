bc4eb327e6cff176ddf1ae9f00eeae5becf250c0
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 00:47:35 +0530

    Create carbon.py

---
+++ Spoiled/SpoiledPlugins/carbon.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+from io import BytesIO
+from aiohttp import ClientSession
+from . import hl, eor, verify 
+
+@Client.on_message(filters.command("carbon", hl))
+async def carbon_func(_, message):
+    x = await verify(_, message)
+    if not x:
+        return
+    if not message.reply_to_message:
+        return await eor(message, "`Reply to a text message to make carbon.`")
+    if not message.reply_to_message.text:
+        return await eor(message, "`Reply to a text message to make carbon.`")
+    m = await eor(message, "`Preparing Carbon`")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("`Uploading`")
+    await _.send_photo(message.chat.id, carbon)
+    await m.delete()
+    carbon.close()
+
+aiosession = ClientSession()
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
