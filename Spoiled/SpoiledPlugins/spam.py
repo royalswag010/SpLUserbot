8a6ba1da1f6f129c4e7b1f515a4a2a14ed85bf3f
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 15:41:30 +0530

    Update spam.py

--- Spoiled/SpoiledPlugins/spam.py
+++ Spoiled/SpoiledPlugins/spam.py
@@ -1,1 +1,14 @@
+from . import hl, eor, verify, get_reply_and_args
+from pyrogram import Client, filters
+import asyncio
 
+@Client.on_message(filters.command("spam", hl))
+async def spam_func(_, m):
+    x = await verify(_, m)
+    if not x:
+        return
+    a, b = await get_reply_and_args(m)
+    if not a and not b:
+        return await eor(m, f"{hl}spam [either reply or give some text]")
+    if not b:
+        return await eor(m, f"{hl}spam [count] [text]")
