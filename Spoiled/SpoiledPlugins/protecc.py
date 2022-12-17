5e55b629d5bf22d47ebfabb2f363ddbe21d1b1cc
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 18:56:46 +0530

    Update protecc.py

--- Spoiled/SpoiledPlugins/protecc.py
+++ Spoiled/SpoiledPlugins/protecc.py
@@ -1,2 +1,7 @@
 from Spoiled.Database.protecc import *
 from pyrogram import Client, filters
+from . import hl
+
+@Client.on_message(filters.command(["autowaifu", "aw"], hl) & filters.group & filters.me)
+async def waifer(_, m):
+    
