1335fe37c13c677ddc87cf0f9bc39b6bc9034b42
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 13:26:38 +0530

    Update protecc.py

--- Spoiled/SpoiledPlugins/protecc.py
+++ Spoiled/SpoiledPlugins/protecc.py
@@ -1,6 +1,6 @@
 from Spoiled.Database.protecc import *
 from pyrogram import Client, filters
-from . import hl, eor
+from . import hl, eor, verify
 from .watchers import protecc_watcher
 import requests
 from bs4 import BeautifulSoup
@@ -11,8 +11,11 @@ husbando_id = 1964681186
 
 waifu_id = 1733263647
 
-@Client.on_message(filters.command(["autowaifu", "aw"], hl) & filters.group & filters.me)
+@Client.on_message(filters.command(["autowaifu", "aw"], hl) & filters.group)
 async def waifer(_, m):
+    x = await verify(_, m)
+    if not x:
+        return
     if len(m.command) > 1:
         try:
             id = (m.text.split()[1])
@@ -30,8 +33,11 @@ async def waifer(_, m):
     x = (await _.get_chat(id)).title
     await eor(m, f"{x} is added to waifu chats !")
 
-@Client.on_message(filters.command(["rmautowaifu", "rmaw"], hl) & filters.group & filters.me)
+@Client.on_message(filters.command(["rmautowaifu", "rmaw"], hl) & filters.group)
 async def rmwaifer(_, m):
+    x = await verify(_, m)
+    if not x:
+        return
     if len(m.command) > 1:
         try:
             id = (m.text.split()[1])
@@ -49,8 +55,11 @@ async def rmwaifer(_, m):
     x = (await _.get_chat(id)).title
     await eor(m, f"{x} is removed from waifu chats !")
 
-@Client.on_message(filters.command(["autohusbando", "ah"], hl) & filters.group & filters.me)
+@Client.on_message(filters.command(["autohusbando", "ah"], hl) & filters.group)
 async def husbander(_, m):
+    x = await verify(_, m)
+    if not x:
+        return
     if len(m.command) > 1:
         try:
             id = (m.text.split()[1])
@@ -68,8 +77,11 @@ async def husbander(_, m):
     x = (await _.get_chat(id)).title
     await eor(m, f"{x} is added to husbando chats !")
 
-@Client.on_message(filters.command(["rmautohusbando", "rmah"], hl) & filters.group & filters.me)
+@Client.on_message(filters.command(["rmautohusbando", "rmah"], hl) & filters.group)
 async def rmhusbander(_, m):
+    x = await verify(_, m)
+    if not x:
+        return
     if len(m.command) > 1:
         try:
             id = (m.text.split()[1])
