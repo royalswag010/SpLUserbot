218aa2ff87fca2193f51770c65b98d6095306ba6
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 21:20:17 +0530

    Update inline.py

--- Spoiled/BotPlugins/inline.py
+++ Spoiled/BotPlugins/inline.py
@@ -1,6 +1,6 @@
 from pyrogram.types import InlineQueryResultPhoto as IQRP
 from Spoiled.SpoiledPlugins import build_help_markup, COMMANDS_HELP
-from pyrogram import Client
+from Spoiled import BOT
 
 URL = "https://telegra.ph/file/c363fbe2341ae115bd8c1.jpg"
 
@@ -8,7 +8,7 @@ ma = build_help_markup(COMMANDS_HELP)
 
 ans = [IQRP(photo_url=URL, thumb_url=URL, title="Help", description="Help Module [SPL-UB]", caption="Select from below buttons !", reply_markup=ma)]
 
-@Client.on_inline_query()
+@BOT.on_inline_query()
 async def inl(_, i):
     text = i.query.lower()
     try:
