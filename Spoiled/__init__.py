972cf8a45ce900ca9f5fc8680c65c549ca1a8b4f
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 22:09:19 +0530

    Update __init__.py

--- Spoiled/__init__.py
+++ Spoiled/__init__.py
@@ -3,4 +3,4 @@ from config import API, SECRETS
 
 SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION)
 
-BOT = Client(":SPL-BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=SECRETS.BOT_TOKEN)
+BOT = Client(":SPL-BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=SECRETS.BOT_TOKEN, plugins=dict(root="Spoiled/BotPlugins"))
