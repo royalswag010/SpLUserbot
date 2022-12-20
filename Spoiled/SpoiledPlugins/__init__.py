d9e38b27e77b96916e551a574f4d3d50404d8aa6
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 11:45:43 +0530

    Update __init__.py

--- Spoiled/SpoiledPlugins/__init__.py
+++ Spoiled/SpoiledPlugins/__init__.py
@@ -1,6 +1,7 @@
 import time
 from config import STUFF
 from Spoiled.Utils import *
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 
 hl = STUFF.COMMAND_HANDLER
 
@@ -11,3 +12,30 @@ COMMANDS_HELP = {}
 def add_command(command, help):
     global COMMANDS_HELP
     COMMANDS_HELP[command] = help
+
+def build_help_markup(set):
+    le = len(set)
+    rows = le // 3
+    rows += 1
+    rem = le % 3
+    buttons = []
+    y = []
+    a = 0
+    filled = 0
+    for each in set:
+        x = IKB(each, callback_data=set[each].lower())
+        y.append(x)
+        a += 1
+        if a == 3:
+            buttons.append(y)
+            filled += 1
+            a = 0
+            y = []
+        if filled == (rows - 1):
+            if set.index(each) == (le - 1):
+                buttons.append(y)
+                y = []
+    final = IKM(buttons)
+    return final
+                
+    
