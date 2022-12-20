3781f7c8776033a0135dbe106297959e39325625
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 20:48:36 +0530

    Create __init__.py

---
+++ Spoiled/__init__.py
@@ -0,0 +1,6 @@
+from pyrogram import Client
+from config import API, SECRETS
+
+SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION)
+
+BOT = Client(":SPL-BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=SECRETS.BOT_TOKEN)
