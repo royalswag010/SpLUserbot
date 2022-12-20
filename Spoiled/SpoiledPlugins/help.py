45ccca50e963a60f2dd3be8bdeadcd44854c4ce4
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 23:10:28 +0530

    Update help.py

--- Spoiled/SpoiledPlugins/help.py
+++ Spoiled/SpoiledPlugins/help.py
@@ -3,21 +3,24 @@ from . import verify, hl, eor
 import time
 from Spoiled import BOT
 
+un = None
+
 @Client.on_message(filters.command("help", hl))
 async def helper_func(_, m):
+    global un
     x = await verify(_, m)
     if not x:
         return
     ok = await eor(m, "💎")
-    time.sleep(3)
     try:
-        await ok.delete()
-        await m.delete()
-    except:
-        pass
-    try:
-        un = (await BOT.get_me()).username
+        if not un:
+            un = (await BOT.get_me()).username
         nice = await _.get_inline_bot_results(bot=un, query="inline_help")
+        try:
+            await ok.delete()
+            await m.delete()
+        except:
+            pass
         await _.send_inline_bot_result(m.chat.id, nice.query_id, nice.results[0].id)
     except Exception as e:
         await eor(m, e)
