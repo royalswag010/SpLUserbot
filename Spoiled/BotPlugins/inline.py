1b200e544587edf82d53791995caac251be4b2b9
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 20:24:41 +0530

    Update inline.py

--- Spoiled/BotPlugins/inline.py
+++ Spoiled/BotPlugins/inline.py
@@ -6,13 +6,13 @@ URL = "https://telegra.ph/file/c363fbe2341ae115bd8c1.jpg"
 
 ma = build_help_markup(COMMANDS_HELP)
 
-ans = [IQRP(photo_url=URL, thumb_url=URL, title="Help", description="dhanush op", caption="Ja Na Lawde", reply_markup=ma)]
+ans = [IQRP(photo_url=URL, thumb_url=URL, title="Help", description="Help Module [SPL-UB]", caption="Select from below buttons !", reply_markup=ma)]
 
 @Client.on_inline_query()
 async def inl(_, i):
+    text = i.query.lower()
     try:
-        text = i.query.lower()
-        if text == "":
-            await _.answer_inline_query(i.id, results=ans, cache_time=0)
+        if text == "inline_help":
+            await _.answer_inline_query(i.id, results=ans, cache_time=0)     
     except Exception as e:
         print(e)
