5543641a54d0170fe7124c3a999845c7e5c5fd82
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 23:02:52 +0530

    Update __init__.py

--- Spoiled/__init__.py
+++ Spoiled/__init__.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 from config import API, SECRETS
 
-SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION)
+SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION, plugins=dict(root="Spoiled/SpoiledPlugins"))
 
 BOT = Client(":SPL-BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=SECRETS.BOT_TOKEN, plugins=dict(root="Spoiled/BotPlugins"))
