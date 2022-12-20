65726c53d91db3c79c5d16ccd845aef14cd0aaa6
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 22:22:57 +0530

    Update callbacks.py

--- Spoiled/BotPlugins/callbacks.py
+++ Spoiled/BotPlugins/callbacks.py
@@ -3,6 +3,7 @@ from Spoiled.SpoiledPlugins import COMMANDS_HELP
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 from .inline import ma
 from pyrogram import Client as BOT
+from Spoiled.SpoiledPlugins import verify
 
 back = IKM([[IKB("Back", callback_data="cmd_back")]])
 
@@ -14,5 +15,11 @@ for x in COMMANDS_HELP:
 
 @BOT.on_callback_query(filters.regex("cmd_back"))
 async def cmd_back(_, q):
-    await q.answer()
-    await q.edit_message_text("Select from below buttons !", reply_markup=ma)
+    h = await verify(_, q.message)
+    if not h:
+        return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
+    try:
+        await q.answer()
+        await q.edit_message_text("Select from below buttons !", reply_markup=ma)
+    except Exception as e:
+        await q.message.reply(e)
