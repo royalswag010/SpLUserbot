60bac0dcacf797e6daa0a8c2dda6a31aafb0d294
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-20 11:18:41 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -2,16 +2,21 @@ from pyrogram import Client, idle
 from config import *
 
 SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION, plugins=dict(root="Spoiled/SpoiledPlugins"))
+BOT = Client(":SPL-BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=SECRETS.BOT_TOKEN)
 
 def initiate():
     SPL.start()
+    BOT.start()
     x = SPL.get_me()
+    y = BOT.get_me()
     try:
         SPL.join_chat("SpLBots")
         SPL.join_chat("Coding_bots")
     except:
         pass
     un = x.username
+    bun = y.username
+    print(f"@{bun} started successfully !")
     print(f"@{un} started successfully !")
     idle()
 
