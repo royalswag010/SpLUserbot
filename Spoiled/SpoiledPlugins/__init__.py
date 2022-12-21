6e55f9eec1d6a06bb821c1d340466a5f0be06628
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-22 00:57:26 +0530

    Update __init__.py

--- Spoiled/SpoiledPlugins/__init__.py
+++ Spoiled/SpoiledPlugins/__init__.py
@@ -2,7 +2,6 @@ import time
 from config import STUFF
 from Spoiled.Utils import *
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
-from .afk import get_readable_time
 
 hl = STUFF.COMMAND_HANDLER
 
@@ -13,6 +12,29 @@ COMMANDS_HELP = {}
 def add_command(command, help):
     global COMMANDS_HELP
     COMMANDS_HELP[command] = help
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
 
 def build_help_markup(set):
     le = len(set)
