3a509a5b3a0a99d339f63e019500d50a72ebbbf3
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-21 09:37:21 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, idle
 from config import *
-from Spoiled import BOT, SPL
+from Spoiled import BOT
+
+SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION, plugins=dict(root="Spoiled/SpoiledPlugins"))
 
 def initiate():
     SPL.start()
