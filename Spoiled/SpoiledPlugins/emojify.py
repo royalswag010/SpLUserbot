01660c294e3272b898eed21ded6a7571523cd67b
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-22 14:43:05 +0530

    Create emojify.py

---
+++ Spoiled/SpoiledPlugins/emojify.py
@@ -0,0 +1,43 @@
+from pyrogram import Client as End, filters
+from pyrogram.types import Message as Dev 
+from Spoiled.Addons.emotes import *
+from . import hl, eor, verify
+
+@End.on_message(filters.command("emojify", hl))
+async def doli(ailika, jhulika: Dev):
+        if not await verify(_, m):
+            return
+        txt = jhulika.text
+        if len(jhulika.command) != 2:
+            return await jhulika.reply("Try: < /emojify Crystal >")
+        txt = txt.split(None, 1)[1]
+        final = ""
+        for a in txt:
+            a = a.lower()
+            a = str(a)
+            if a in END_TEXT:
+                letter = END_EMOJI[END_TEXT.index(a)]
+                final += letter
+            else:
+                final += a
+        await eor(m, final)
+
+@End.on_message(filters.command("crystal", hl))
+async def crystal(ailika, jhulika: Dev):
+    if not await verify(_, m):
+        return
+    txt = jhulika.text
+    if len(jhulika.command)!= 3:
+        return await jhulika.reply("Try: < /crystal ✨ doli >")
+    emoji = txt.split(None, 2)[1]
+    text = txt.split(None, 2)[2]
+    final = ""
+    for a in text:
+        a = a.lower()
+        a = str(a)
+        if a in END_TEXT:
+            letter = END_CJ[END_TEXT.index(a)].format(cj=emoji)
+            final += letter
+        else:
+            final += a
+    await eor(m, final)
