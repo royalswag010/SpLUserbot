222a813b8e8e295739392a4154dad37680b078d4
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-23 13:32:20 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,16 +1,21 @@
 from pyrogram import Client, idle
 from config import *
+import time 
 from Spoiled import BOT, SPL
+from pyrogram.errors import FloodWait
 
 # SPL = Client(":SPL-USERBOT:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=SECRETS.STRING_SESSION, plugins=dict(root="Spoiled/SpoiledPlugins"))
 
 # BOT = Client(":SPL-BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=SECRETS.BOT_TOKEN, plugins=dict(root="Spoiled/BotPlugins"))
 
 def initiate():
-    SPL.start()
-    BOT.start()
-    x = SPL.get_me()
-    y = BOT.get_me()
+    try:
+        SPL.start()
+        BOT.start()
+        x = SPL.get_me()
+        y = BOT.get_me()
+    except FloodWait as e:
+        time.sleep(e.value)
     try:
         SPL.join_chat("SpLBots")
         SPL.join_chat("Coding_bots")
