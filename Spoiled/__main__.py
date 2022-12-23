29ef5e9451f8df58b7a3aab6cb29e6dd2d82a66f
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-23 14:35:11 +0530

    Update __main__.py

--- Spoiled/__main__.py
+++ Spoiled/__main__.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait
 import asyncio
 
 async def initiate():
-   """
+    """
     try:
         await BOT.start()
         print("Bot started !")
@@ -14,7 +14,7 @@ async def initiate():
     except FloodWait as e:
         print("Can't start bot, sleeping...")
         await asyncio.sleep(e.value)
-   """
+    """
     try:
         await SPL.start()
         print("Userbot started !")
