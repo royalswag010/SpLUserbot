ef5d8efb745a654d3ed5df2903df479675bb2532
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-23 14:00:53 +0530

    Update __main__.py

--- Spoiled/__main__.py
+++ Spoiled/__main__.py
@@ -7,9 +7,16 @@ import asyncio
 
 async def initiate():
     try:
-        await SPL.start()
         await BOT.start()
+        print("Bot started !")
     except FloodWait as e:
+        print("Can't start bot, sleeping...")
+        await asyncio.sleep(e.value)
+    try:
+        await SPL.start()
+        print("Userbot started !")
+    except FloodWait as e:
+        print(Can't start userbot, sleeping...")
         await asyncio.sleep(e.value)
     try:
         await SPL.send_message(CHATS.LOG_GROUP_ID, "Ub started !")
