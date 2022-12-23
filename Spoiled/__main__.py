235ccafe66bbb67fdea7b60c5dfae8f6a560452f
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-23 13:37:45 +0530

    Update __main__.py

--- Spoiled/__main__.py
+++ Spoiled/__main__.py
@@ -1,12 +1,18 @@
 from Spoiled import SPL, BOT
 from pyrogram import idle
 from config import CHATS
+from pyrogram.errors import FloodWait
 
 import asyncio
 
 async def initiate():
-    await SPL.start()
-    await BOT.start()
+    try:
+        SPL.start()
+        BOT.start()
+        x = SPL.get_me()
+        y = BOT.get_me()
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
     try:
         await SPL.send_message(CHATS.LOG_GROUP_ID, "Ub started !")
         await BOT.send_message(CHATS.LOG_GROUP_ID, "Bot started !")
