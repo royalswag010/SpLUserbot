750a5328d97f1366483376c3559afe0bd0f78627
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-24 19:40:22 +0530

    Update __init__.py

--- Spoiled/SpoiledPlugins/__init__.py
+++ Spoiled/SpoiledPlugins/__init__.py
@@ -2,9 +2,28 @@ import time
 from config import STUFF
 from Spoiled.Utils import *
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
+import glob
+from os.path import dirname, isfile
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
+
+    all_modules = [
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f)
+        and f.endswith(".py")
+        and not f.endswith("__init__.py")
+    ]
+
+    return all_modules
+
+SPL_MODULES = sorted(__list_all_modules())
+__all__ = SPL_MODULES + ["SPL_MODULES"]
 
 hl = STUFF.COMMAND_HANDLER
-#
+
 startTime = time.time()
 
 COMMANDS_HELP = {}
