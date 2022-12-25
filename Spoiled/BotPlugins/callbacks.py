576da2cee8f6d7dd86444927cc19024f8953da6d
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-25 09:52:43 +0530

    Update callbacks.py

--- Spoiled/BotPlugins/callbacks.py
+++ Spoiled/BotPlugins/callbacks.py
@@ -10,19 +10,19 @@ back = IKM([[IKB("Back", callback_data="cmd_back")]])
 
 id = None
 
-while True:
-    for x in COMMANDS_HELP:
-        @BOT.on_callback_query(filters.regex(x.lower()))
-        async def cbq_alpha(_, q):
-            global id
-            if not id:
-                id = (await SPL.get_me()).id
-            qid = q.from_user.id
-            if id != qid:
-                if not await is_sudo(qid):
-                    return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
-            await q.answer()
-            await q.edit_message_text(COMMANDS_HELP[x], reply_markup=back)
+
+for x in COMMANDS_HELP:
+    @BOT.on_callback_query(filters.regex(x.lower()))
+    async def cbq_alpha(_, q):
+        global id
+        if not id:
+            id = (await SPL.get_me()).id
+        qid = q.from_user.id
+        if id != qid:
+            if not await is_sudo(qid):
+                return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
+        await q.answer()
+        await q.edit_message_text(COMMANDS_HELP[x], reply_markup=back)
 
 @BOT.on_callback_query(filters.regex("cmd_back"))
 async def cmd_back(_, q):
