9b48f5b80e82d9fc86f6ba437c52e16ff1266803
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-25 10:02:54 +0530

    Update callbacks.py

--- Spoiled/BotPlugins/callbacks.py
+++ Spoiled/BotPlugins/callbacks.py
@@ -10,19 +10,45 @@ back = IKM([[IKB("Back", callback_data="cmd_back")]])
 
 id = None
 
-
+CMDS = []
 for x in COMMANDS_HELP:
-    @BOT.on_callback_query(filters.regex(x.lower()))
-    async def cbq_alpha(_, q):
-        global id
-        if not id:
-            id = (await SPL.get_me()).id
-        qid = q.from_user.id
-        if id != qid:
-            if not await is_sudo(qid):
-                return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
-        await q.answer()
-        await q.edit_message_text(COMMANDS_HELP[x], reply_markup=back)
+    CMDS.append(x)
+
+@BOT.on_callback_query(filters.regex(CMDS[0].lower()))
+async def cbq_0(_, q):
+    global id
+    if not id:
+        id = (await SPL.get_me()).id
+    qid = q.from_user.id
+    if id != qid:
+        if not await is_sudo(qid):
+            return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
+    await q.answer()
+    await q.edit_message_text(COMMANDS_HELP[CMDS[0]], reply_markup=back)
+
+BOT.on_callback_query(filters.regex(CMDS[1].lower()))
+async def cbq_1(_, q):
+    global id
+    if not id:
+        id = (await SPL.get_me()).id
+    qid = q.from_user.id
+    if id != qid:
+        if not await is_sudo(qid):
+            return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
+    await q.answer()
+    await q.edit_message_text(COMMANDS_HELP[CMDS[1]], reply_markup=back)
+
+BOT.on_callback_query(filters.regex(CMDS[2].lower()))
+async def cbq_2(_, q):
+    global id
+    if not id:
+        id = (await SPL.get_me()).id
+    qid = q.from_user.id
+    if id != qid:
+        if not await is_sudo(qid):
+            return await q.answer("You can't use these !\n\nCreate your own.", show_alert=True)
+    await q.answer()
+    await q.edit_message_text(COMMANDS_HELP[CMDS[2]], reply_markup=back)
 
 @BOT.on_callback_query(filters.regex("cmd_back"))
 async def cmd_back(_, q):
