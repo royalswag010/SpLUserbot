7ad16b6bc7d8c6cd1e6ec173218def8b444f3137
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-25 19:07:29 +0530

    Update spam.py

--- Spoiled/SpoiledPlugins/spam.py
+++ Spoiled/SpoiledPlugins/spam.py
@@ -2,8 +2,15 @@ from . import hl, eor, verify, get_reply_and_args
 from pyrogram import Client, filters
 import asyncio
 
+SPAM = False
+
+STOP = True
+
 @Client.on_message(filters.command("spam", hl))
 async def spam_func(_, m):
+    global SPAM, STOP
+    STOP = False
+    SPAM = True
     reply = m.reply_to_message
     x = await verify(_, m)
     if not x:
@@ -45,38 +52,59 @@ async def spam_func(_, m):
     if type == "photo":
         id = reply.photo.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_photo(m.chat.id, id, caption=f"{txt if caption else blank}")
     elif type == "video":
         id = reply.video.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_video(m.chat.id, id, caption=f"{txt if caption else blank}")
     elif type == "document":
         id = reply.document.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_document(m.chat.id, id, caption=f"{txt if caption else blank}", force_document=True)
     elif type == "animation":
         id = reply.animation.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_animation(m.chat.id, id, caption=f"{txt if caption else blank}")
     elif type == "voice":
         id = reply.voice.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_voice(m.chat.id, id, caption=f"{txt if caption else blank}")
     elif type == "audio":
         id = reply.audio.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_audio(m.chat.id, id, caption=f"{txt if caption else blank}")
     elif type == "text":
         id = reply.text
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_message(m.chat.id, id)
     elif type == "sticker":
         id = reply.sticker.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_sticker(m.chat.id, id)
+    STOP = True
+    SPAM = False
 
 @Client.on_message(filters.command(["dspam", "delayspam"], hl))
 async def dspam_func(_, m):
+    global SPAM, STOP
+    STOP = False
+    SPAM = True
     reply = m.reply_to_message
     x = await verify(_, m)
     if not x:
@@ -121,40 +149,71 @@ async def dspam_func(_, m):
     if type == "photo":
         id = reply.photo.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_photo(m.chat.id, id, caption=f"{txt if caption else blank}")
             await asyncio.sleep(delay)
     elif type == "video":
         id = reply.video.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_video(m.chat.id, id, caption=f"{txt if caption else blank}")
             await asyncio.sleep(delay)
     elif type == "document":
         id = reply.document.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_document(m.chat.id, id, caption=f"{txt if caption else blank}", force_document=True)
             await asyncio.sleep(delay) 
     elif type == "animation":
         id = reply.animation.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_animation(m.chat.id, id, caption=f"{txt if caption else blank}")
             await asyncio.sleep(delay)
     elif type == "voice":
         id = reply.voice.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_voice(m.chat.id, id, caption=f"{txt if caption else blank}")
             await asyncio.sleep(delay)
     elif type == "audio":
         id = reply.audio.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_audio(m.chat.id, id, caption=f"{txt if caption else blank}")
             await asyncio.sleep(delay)
     elif type == "text":
         id = reply.text
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_message(m.chat.id, id)
             await asyncio.sleep(delay)
     elif type == "sticker":
         id = reply.sticker.file_id
         for u in range(0, count):
+            if STOP:
+                return
             await _.send_sticker(m.chat.id, id)
             await asyncio.sleep(delay)
+    STOP = True
+    SPAM = False
+
+@Client.on_message(filters.command("stop", hl) & filters.group)
+async def spam_stop(_, m):
+    global SPAM, STOP
+    if not await verify(_, m):
+        return
+    if not SPAM:
+        return await eor(m, "No ongoing spam...")
+    STOP = True
+    SPAM = False
+    return await eor(m, "Terminated...")
+
+    
