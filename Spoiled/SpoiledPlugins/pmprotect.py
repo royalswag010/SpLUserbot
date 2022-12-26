aa945999f01ce6afd490625fa87eb60628fc56ef
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-26 23:12:28 +0530

    Update pmprotect.py

--- Spoiled/SpoiledPlugins/pmprotect.py
+++ Spoiled/SpoiledPlugins/pmprotect.py
@@ -3,6 +3,10 @@ from . import eor, hl, verify, add_command, get_id
 from Spoiled.Database.pm import *
 from .watchers import pm_watcher
 
+KOWSHIK = "https://telegra.ph/file/e19d5689b1970ce2a89cf.jpg"
+
+TEXT = "Hello {}, I'm assistant bot of {}, This is automated message to protect from spamming my master's DM, so wait until they Approves you to PM them, until then you'll be get warned\n\nIf you get {} warns, you'll get blocked automatically.\n\nCurrent warnings : {}"
+
 @Client.on_message(filters.command("pmprotect", hl) & filters.me)
 async def pmpro(_, m):
     x = await is_pm_on()
@@ -16,6 +20,8 @@ async def pmpro(_, m):
         if x:
             return await eor(m, "PM PROTECTION ALREADY ENABLED !")
         await toggle_pm()
+        if await limit() == 0:
+            await update_warns(3)
         return await eor(m, "PM PROTECTION ENABLED !")
     if not x:
         return await eor(m, "PM PROTECTION ISN'T ENABLED !")
@@ -61,3 +67,9 @@ async def cwf(_, m):
         return
     if is_approved(m.from_user.id):
         return
+    await warn_user(m.from_user.id)
+    if await limit() <= await get_warns(m.from_user.id):
+        await m.reply("GOOD BYE UNTIL MY MASTER ARRIVES !")
+        await _.block_user(m.from_user.id)
+    await m.reply_photo(KOWSHIK, TEXT.format(m.from_user.first_name, (await _.get_me()).first_name, await limit(), await get_warns(m.from_user.id)))
+ 
