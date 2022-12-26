b29a31af97989a4a09b1953d0d15997b3cf763ec
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-26 21:46:02 +0530

    Update pm.py

--- Spoiled/Database/pm.py
+++ Spoiled/Database/pm.py
@@ -2,6 +2,7 @@ from . import db
 
 pmdb = db.pm
 pmwarn = db.pmwarn
+pmap = db.pmapprove
 
 async def toggle_pm():
     x = await pmdb.find_one({"pm": 0})
@@ -19,3 +20,20 @@ async def is_pm_on():
 async def update_warns(w: int):
     await pmwarn.update_one({"w": "w"}, {"$set": {"warns": w}}, upsert=True)
 
+async def approve(user_id: int):
+    x = await pmap.find_one({"user_id": user_id})
+    if x:
+        return
+    await pmap.insert_one({"user_id": user_id})
+
+async def disapprove(user_id: int):
+    x = await pmap.find_one({"user_id": user_id})
+    if not x:
+        return
+    await pmap.delete_one({"user_id": user_id})
+
+async def is_approved(user_id: int):
+    x = await pmap.find_one({"user_id": user_id})
+    if x:
+        return True
+    return False
